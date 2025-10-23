532d9bda37b77700400526dba2c37e2d78f035f5
alexmorris0n <alexandermorrison@gmail.com>
2025-10-23 02:29:40 -0700

    Fix AudioBridge import for WebRTC mode

--- bridge/server.js
+++ bridge/server.js
@@ -17,9 +17,9 @@ const fastifyFormbody = require('@fastify/formbody');
 
 // Choose bridge implementation based on environment
 const USE_WEBRTC = process.env.USE_WEBRTC === 'true';
-const { AudioBridge } = USE_WEBRTC 
-  ? require('./audio-bridge-webrtc-simple')
-  : { AudioBridge: require('./audio-bridge-lean') };
+const AudioBridge = USE_WEBRTC 
+  ? require('./audio-bridge-webrtc-simple').AudioBridgeWebRTC
+  : require('./audio-bridge-lean');
 
 console.log(`🔌 Using ${USE_WEBRTC ? 'WebRTC' : 'WebSocket'} bridge`);
 
